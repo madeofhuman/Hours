7ecd3cfd73e4ac749c7e50ea628171094ac323b1
CJ Odina <josephodina@gmail.com>
2019-06-23 16:59:11 +0100

    implement category page to show all projects in a category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,24 @@
 class CategoriesController < ApplicationController
-  before_action :find_category, only: [:edit, :update]
+  before_action :find_category, only: [:show, :edit, :update]
 
   def index
     @category = Category.new
     @categories = Category.by_name
   end
 
+  def show
+    @projects = Project.joins(:categories)
+                       .where('categories.id = ?', @category.id)
+                       .uniq
+                       .unarchived
+                       .by_last_updated
+                       .page(params[:page])
+                       .per(7)
+  end
+
   def create
     @category = Category.new(category_params)
-    if @category.save
+    if @category.sav
       redirect_to categories_path, notice: t(:category_created)
     else
       @categories = Category.by_name
